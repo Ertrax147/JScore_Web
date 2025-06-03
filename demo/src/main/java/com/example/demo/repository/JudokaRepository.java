8bb6fd4e9afb6eceedda03e446a7e97e8b3c610f
AlonsoRom <w.chavez01@ufromail.cl>
2025-06-03 13:21:49 -0400

    Clases Judoka relacionadas

--- demo/src/main/java/com/example/demo/repository/JudokaRepository.java
+++ demo/src/main/java/com/example/demo/repository/JudokaRepository.java
@@ -1,4 +1,24 @@
 package com.example.demo.repository;
 
-public interface JudokaRepository {
+
+import com.example.demo.model.user.Judoka;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+/**
+ * The interface Judoka repository.
+ */
+public interface JudokaRepository extends JpaRepository<Judoka, Long> {
+
+    /**
+     * Find by nombre list.
+     *
+     * @param nombre the nombre
+     * @return the list
+     */
+    List<Judoka> findByNombre(String nombre);
+
+    Optional<Judoka> findByUsername(String username);
+
 }
